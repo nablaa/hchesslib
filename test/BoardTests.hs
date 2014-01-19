c82cf456f6e4fdee2795be58282875faa4780096
Miika-Petteri Matikainen <miikapetteri@gmail.com>
2014-01-19 15:22:17 +0200

    Create new file for Board tests

---
+++ test/BoardTests.hs
@@ -0,0 +1,39 @@
+module BoardTests where
+
+import Board
+import Test.HUnit
+
+boardPrintingTests :: Test
+boardPrintingTests = TestList [
+        "rnbqkbnr\npppppppp\n        \n        \n        \n        \nPPPPPPPP\nRNBQKBNR\n" ~=? printBoardCompact initialBoard
+        ]
+
+boardCoordinateTests :: Test
+boardCoordinateTests = TestList [
+          True ~=? isInsideBoard (0, 0)
+        , True ~=? isInsideBoard (7, 7)
+        , True ~=? isInsideBoard (3, 5)
+        , False ~=? isInsideBoard (-1, 5)
+        , False ~=? isInsideBoard (3, -1)
+        , False ~=? isInsideBoard (8, 5)
+        , False ~=? isInsideBoard (3, 8)
+        , Just (0, 0) ~=? parseCoordinate "a8"
+        , Just (0, 1) ~=? parseCoordinate "b8"
+        , Just (0, 2) ~=? parseCoordinate "c8"
+        , Just (1, 0) ~=? parseCoordinate "a7"
+        , Just (2, 0) ~=? parseCoordinate "a6"
+        , Just (7, 0) ~=? parseCoordinate "a1"
+        , Just (0, 7) ~=? parseCoordinate "h8"
+        , Just (7, 7) ~=? parseCoordinate "h1"
+        , Just (3, 5) ~=? parseCoordinate "f5"
+        , Nothing ~=? parseCoordinate "B5"
+        , Nothing ~=? parseCoordinate "F5"
+        , Nothing ~=? parseCoordinate "foobar"
+        , Nothing ~=? parseCoordinate "12"
+        , Nothing ~=? parseCoordinate "i1"
+        , Nothing ~=? parseCoordinate "a9"
+        , Nothing ~=? parseCoordinate "a-1"
+        ]
+
+boardTests :: Test
+boardTests = TestList [boardPrintingTests, boardCoordinateTests]
